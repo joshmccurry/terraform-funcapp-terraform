fe0791999d98b7a05c1c02e9059023b8d3883aff
Josh McCurry <joshmccurry@microsoft.com>
2022-04-27 15:55:05 -0400

    vnet integration

--- functionapp-test.tf
+++ functionapp-test.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.99.0"
+      version = "3.3.0"
     }
   }
 }
@@ -18,6 +18,10 @@ resource "azurerm_storage_account" "testapp" {
   location                 = azurerm_resource_group.testapp.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  network_rules {
+    default_action             = "Allow"
+    virtual_network_subnet_ids = [azurerm_subnet.testapp.id]
+  }
 }
 
 resource "azurerm_app_service_plan" "testapp" {
@@ -49,6 +53,44 @@ resource "azurerm_function_app" "testapp" {
   app_settings = {
     "WEBSITE_CONTENTSHARE" = azurerm_storage_share.testapp.name
     "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.testapp.primary_connection_string
+    "WEBSITE_CONTENTOVERVNET" = "1"
   }
+  site_config {
+    always_on = "true"
+    vnet_route_all_enabled = "true"
+  }
+}
+
+resource "azurerm_network_security_group" "testapp" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.testapp.location
+  resource_group_name = azurerm_resource_group.testapp.name
+}
+
+resource "azurerm_virtual_network" "testapp" {
+  name                 = format("testvnet-%s", substr(uuid(), 0, 4))
+  location            = azurerm_resource_group.testapp.location
+  resource_group_name = azurerm_resource_group.testapp.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "testapp" {
+  name                 = format("testsubnet-%s", substr(uuid(), 0, 4))
+  resource_group_name  = azurerm_resource_group.testapp.name
+  virtual_network_name = azurerm_virtual_network.testapp.name
+  address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Web", "Microsoft.Storage"]
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+resource "azurerm_app_service_virtual_network_swift_connection" "testapp" {
+  app_service_id = azurerm_function_app.testapp.id
+  subnet_id      = azurerm_subnet.testapp.id
 }
 
